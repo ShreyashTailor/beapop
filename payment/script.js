8fc86f571b15d0f583a1b40a2da525ec616e4aeb
Shreyash Tailor <151995519+Captain1678@users.noreply.github.com>
2024-04-03 08:56:36 +0530

    Update script.js

--- payment/script.js
+++ payment/script.js
@@ -1,29 +1,28 @@
-const razor = ()=>{
-    return window.alert("Soon ^_^")
+const razor = () => {
+    openGPayQRCode();
 }
 
-const paytm = ()=>{
-    return window.alert("Soon ^_^")
+const paytm = () => {
+    openGPayQRCode();
 }
 
-// qr code function
-
+// QR code function
 document.addEventListener('DOMContentLoaded', function() {
-
     let gpayButton = document.getElementById('gpay-pay-button');
     
-   
     gpayButton.addEventListener('click', function() {
-        
         openQRCodeImage();
     });
 });
 
-
 function openQRCodeImage() {
+    let qrCodeImagePath = 'qrcode.png'; 
     
-    let qrCodeImagePath = 'qrcode.png';
-    
+    window.open(qrCodeImagePath, '_blank');
+}
 
+function openGPayQRCode() {
+    let qrCodeImagePath = 'qrcode.png'; 
+    
     window.open(qrCodeImagePath, '_blank');
 }
